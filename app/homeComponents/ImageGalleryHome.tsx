1d8ed6d698cc06c6865567e1b735dcfcab310fcb
donlangdang <donlangdangcc@gmail.com>
2026-01-09 23:13:32 +0700

    remove light gallrey

--- app/homeComponents/ImageGalleryHome.tsx
+++ app/homeComponents/ImageGalleryHome.tsx
@@ -1,31 +1,42 @@
 import Link from "next/link";
 import Image from "next/image";
-import LightGallery from "lightgallery/react";
-
-// import styles
-import "lightgallery/css/lightgallery.css";
-import "lightgallery/css/lg-zoom.css";
-import "lightgallery/css/lg-thumbnail.css";
-
-// import plugins if you need
-import lgThumbnail from "lightgallery/plugins/thumbnail";
-import lgZoom from "lightgallery/plugins/zoom";
 
 const ImageGalleryHome = () => {
   const ImageDemo: string =
     "https://res.cloudinary.com/dhadoo9ui/image/upload/v1764168332/PXL_20250930_034312004_vkpgkf.jpg";
 
-  const onInit = () => {
-    console.log("lightGallery has been initialized");
-  };
-
   return (
     <div className="gap-4 p-4 w-full h-auto">
-      <LightGallery onInit={onInit} speed={500} plugins={[lgThumbnail, lgZoom]}>
-        <Link href={ImageDemo} className="relative">
-          <Image alt="demo" src={ImageDemo} fill className="" />
-        </Link>
-      </LightGallery>
+      {/* <Link href={ImageDemo}> */}
+      {/*   <Image alt="demo" src={ImageDemo} width={150} height={300} /> */}
+      {/* </Link> */}
+      {/* <Link href={ImageDemo}> */}
+      {/*   <Image alt="demo" src={ImageDemo} width={200} height={300} /> */}
+      {/* </Link> */}
+      {/* <Link href={ImageDemo}> */}
+      {/*   <Image alt="demo" src={ImageDemo} width={200} height={300} /> */}
+      {/* </Link> */}
+      {/* <Link href={ImageDemo}> */}
+      {/*   <Image alt="demo" src={ImageDemo} width={200} height={300} /> */}
+      {/* </Link> */}
+      {/* <Link href={ImageDemo}> */}
+      {/*   <Image alt="demo" src={ImageDemo} width={200} height={300} /> */}
+      {/* </Link> */}
+      {/* <Link href={ImageDemo}> */}
+      {/*   <Image alt="demo" src={ImageDemo} width={200} height={300} /> */}
+      {/* </Link> */}
+      {/* <Link href={ImageDemo}> */}
+      {/*   <Image alt="demo" src={ImageDemo} width={200} height={300} /> */}
+      {/* </Link> */}
+      {/* <Link href={ImageDemo}> */}
+      {/*   <Image alt="demo" src={ImageDemo} width={200} height={300} /> */}
+      {/* </Link> */}
+      {/* <Link href={ImageDemo}> */}
+      {/*   <Image alt="demo" src={ImageDemo} width={200} height={300} /> */}
+      {/* </Link> */}
+      {/* <Link href={ImageDemo}> */}
+      {/*   <Image alt="demo" src={ImageDemo} width={200} height={300} /> */}
+      {/* </Link> */}
     </div>
   );
 };
--- app/product-list/page.tsx
+++ app/product-list/page.tsx
@@ -1,14 +1,5 @@
-import { LightGallery } from "lightgallery/lightgallery";
-
-import "lightgallery/css/lightgallery.css";
-import "lightgallery/css/lg-zoom.css";
-import "lightgallery/css/lg-thumbnail.css";
-
-import lgThumbnail from "lightgallery/plugins/thumbnail";
-import lgZoom from "lightgallery/plugins/zoom";
-
 const page = () => {
-  return <div>product list </div>;
+  return <div className="h-3000">product list </div>;
 };
 
 export default page;
