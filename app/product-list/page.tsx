22a9bc1fb511e97acb84867beb3c660e609c5fa5
donlangdang <donlangdangcc@gmail.com>
2026-03-29 22:11:22 +0700

    new commit for product-list

--- app/product-list/page.tsx
+++ app/product-list/page.tsx
@@ -1,5 +1,10 @@
 const page = () => {
-  return <div className="h-3000">product list </div>;
+  return (
+    <div className="h-3000">
+      product list
+      <div className="w-auto h-screen">products list draw </div>
+    </div>
+  );
 };
 
 export default page;
