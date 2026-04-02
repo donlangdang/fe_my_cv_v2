9f39606ce76319ac4c0c436ae8eaf4a790234ae9
donlangdang <donlangdangcc@gmail.com>
2026-04-02 22:19:32 +0700

    making layout and idea for zoom scroll for galery images

--- app/product-list/page.tsx
+++ app/product-list/page.tsx
@@ -1,8 +1,16 @@
+import YearGalery from "./yearGalery/YearGalery";
+
 const page = () => {
   return (
-    <div className="h-3000">
-      product list
-      <div className="w-auto h-screen">products list draw </div>
+    <div className="h-3000 pt-21 px-6 md:px-16 md:pt-24">
+      <YearGalery />
+      <div className="w-auto ">2025</div>
+      <div className="w-auto ">2024</div>
+      <div className="w-auto ">2023</div>
+      <div className="w-auto ">2022</div>
+      <div className="w-auto ">2021</div>
+      <div className="w-auto ">2020</div>
+      <div className="w-auto ">2019</div>
     </div>
   );
 };
---
+++ app/product-list/yearGalery/YearGalery.tsx
@@ -0,0 +1,5 @@
+const YearGalery = () => {
+  return <div>2026</div>;
+};
+
+export default YearGalery;
