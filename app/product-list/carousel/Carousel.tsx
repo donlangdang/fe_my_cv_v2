7d6d833ab62ba3b28ef1b95bfb25376bf9d306f0
donlangdang <donlangdangcc@gmail.com>
2026-04-13 23:11:39 +0700

    making carosel imagee in product list

---
+++ app/product-list/carousel/Carousel.tsx
@@ -0,0 +1,36 @@
+import Image from "next/image";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+const CarouselListImages = () => {
+  return (
+    <Carousel className="w-full sm:w-2/3 h-auto">
+      <CarouselContent>
+        <CarouselItem className="relative w-full h-auto aspect-square">
+          <Image
+            alt="product and project image"
+            fill
+            loading="lazy"
+            className="object-center object-contain"
+            src="/bannerHome2.1.jpg"
+          />
+        </CarouselItem>
+        <CarouselItem className="relative w-full h-auto aspect-square">
+          <Image
+            alt="product and project image"
+            fill
+            loading="lazy"
+            className="object-center object-contain"
+            src="/bannerHome2.1.jpg"
+          />
+        </CarouselItem>
+      </CarouselContent>
+    </Carousel>
+  );
+};
+
+export default CarouselListImages;
--- app/product-list/yearGalery/YearGalery.tsx
+++ app/product-list/yearGalery/YearGalery.tsx
@@ -1,5 +1,13 @@
 "use client";
 import { motion } from "framer-motion";
+import Image from "next/image";
+import Link from "next/link";
+import {
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
 const YearGalery = () => {
   const a = [
@@ -16,9 +24,31 @@ const YearGalery = () => {
           whileHover={{ scale: 1.03 }}
           initial={{ opacity: 0, scale: 0.7 }}
           whileInView={{ opacity: 1, scale: 1, transition: { duration: 0.5 } }}
-          className="w-full h-auto aspect-3/4 rounded-2xl bg-muted"
+          className="relative w-full h-auto aspect-3/4 rounded-2xl bg-muted"
         >
-          {b}
+          <Link href="#" className=" w-full h-full overflow-hidden">
+            <Dialog>
+              <DialogTrigger asChild>
+                <Image
+                  alt="product and project Link"
+                  fill
+                  loading="lazy"
+                  className="object-center object-cover"
+                  src="/bannerHome2.1.jpg"
+                />
+              </DialogTrigger>
+              <DialogContent className="flex items-center justify-center sm:max-w-8xl w-full h-full max-h-[calc(100%-2rem)]">
+                <DialogTitle className="hidden"></DialogTitle>
+                <Image
+                  alt="product and project image"
+                  fill
+                  loading="lazy"
+                  className="object-center object-contain"
+                  src="/bannerHome2.1.jpg"
+                />
+              </DialogContent>
+            </Dialog>
+          </Link>
         </motion.div>
       ))}
     </div>
